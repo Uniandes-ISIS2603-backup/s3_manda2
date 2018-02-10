09c7dc845bfee49782cf023e0911dda70e4a1cd4
Diego Ramos <da.ramos@uniandes.edu.co>
2018-02-10 15:08:02 -0500

    DTOS y Resources de Servicio y PayPal

---
+++ s3_manda2-web/src/main/java/co/edu/uniandes/csw/manda2/dtos/ServicioDetailDTO.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.manda2.dtos;
+
+/**
+ * Clase que representa un servicio de la aplicación en detalle.
+ * @author da.ramos
+ */
+public class ServicioDetailDTO extends ServicioDTO{
+    
+    /**
+     * Pago asociado al servicio.
+     */
+    private PagoDTO pago;
+    
+    /**
+     * Cliente que pide el servicio.
+     */
+    private ClienteDTO cliente;
+    
+    /**
+     * Empleado que realiza el servicio.
+     */
+    private EmpleadoDTO empleado;
+
+    /**
+     * Retorna el pago del servicio.
+     * @return Pago del servicio.
+     */
+    public PagoDTO getPago() {
+        return pago;
+    }
+
+    /**
+     * Asigna el pago del servicio al dado por parámetro.
+     * @param pago Nuevo pago del servicio.
+     */
+    public void setPago(PagoDTO pago) {
+        this.pago = pago;
+    }
+
+    /**
+     * Retorna el cliente que pide el servicio.
+     * @return Cliente que pide el servicio.
+     */
+    public ClienteDTO getCliente() {
+        return cliente;
+    }
+
+    /**
+     * Asigna el cliente que pide el servicio al dado por parámetro.
+     * @param cliente Nuevo cliente que pide el servicio.
+     */
+    public void setCliente(ClienteDTO cliente) {
+        this.cliente = cliente;
+    }
+
+    /**
+     * Retorna el empleado que realiza el servicio.
+     * @return Empleado que realiza el servicio.
+     */
+    public EmpleadoDTO getEmpleado() {
+        return empleado;
+    }
+
+    /**
+     * Asigna el empleado que realiza el servicio al dado por parámetro.
+     * @param empleado Nuevo empleado que realiza el servicio.
+     */
+    public void setEmpleado(EmpleadoDTO empleado) {
+        this.empleado = empleado;
+    }
+}
---
+++ s3_manda2-web/src/main/java/co/edu/uniandes/csw/manda2/resources/PayPalResource.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.manda2.resources;
+
+import co.edu.uniandes.csw.manda2.dtos.PayPalDTO;
+import java.util.List;
+import javax.ws.rs.*;
+
+/**
+ *
+ * @author da.ramos
+ */
+@Path("paypal")
+public class PayPalResource {
+    
+    @GET
+    public List<PayPalDTO> getPayPals(){
+        return null;
+    }
+    
+    @GET
+    @Path("{id : \\d+}")
+    public PayPalDTO getPayPal(@PathParam("id") long id){
+        return null;
+    }
+    
+    @POST
+    public PayPalDTO createPayPal(PayPalDTO payPal){
+        return null;
+    }
+    
+    @PUT
+    @Path("{id : \\d+}")
+    public PayPalDTO updatePayPal(@PathParam("id") long id, PayPalDTO payPal){
+        return null;
+    }
+    
+    @DELETE
+    @Path("{id : \\d+}")
+    public void deletePayPal(@PathParam("id") long id){
+        
+    }
+}
