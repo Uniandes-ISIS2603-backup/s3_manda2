83d3673f122258106b84cb7579dba01a2a9e54d1
Diego Ramos <da.ramos@uniandes.edu.co>
2018-02-13 11:28:59 -0500

    Corregido error en el package de OrganizacionDetailsDTO

--- s3_manda2-web/src/main/java/co/edu/uniandes/csw/manda2/dtos/OrganizacionDetailsDTO.java
+++ s3_manda2-web/src/main/java/co/edu/uniandes/csw/manda2/dtos/OrganizacionDetailsDTO.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package co.edu.uniandes.csw.manda2.DTOS;
+package co.edu.uniandes.csw.manda2.dtos;
 
 /**
  *
