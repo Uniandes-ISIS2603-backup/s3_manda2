74c95f8c10e3e674a336aa5a9965bcca1d671a7e
NICOLAS BELLO LUGO <n.bello@sis.virtual.uniandes.edu.co>
2018-02-15 11:26:36 -0500

    se terminan la clases DTO con todo lo necesario

--- s3_manda2-web/src/main/java/co/edu/uniandes/csw/manda2/dtos/EmpleadoDetailDTO.java
+++ s3_manda2-web/src/main/java/co/edu/uniandes/csw/manda2/dtos/EmpleadoDetailDTO.java
@@ -8,7 +8,53 @@ package co.edu.uniandes.csw.manda2.dtos;
 import java.util.ArrayList;
 
 /**
- *
+ * Clase que representa el empleado.
+ * 
+ * Al serializarse como JSON esta clase implementa el siguiente modelo: <br>
+ * <pre>
+ *   {
+ *      "nombre": string, 
+ *      "cedula": string,
+ *      "fechaingreso": date,
+ *      "calificacion": number,
+ *      "id": number,
+ *      "identificacion": string,
+ *      "foto": string,
+ *      "eps: string,
+ *      "pagos" [{}],
+ *      "servicios" [{}]
+ *   }
+ * </pre>
+ * Por ejemplo un empleado se representa así:<br>
+ * 
+ * <pre>
+ *   {
+ *      "nombre": "nicolas_caceres", 
+ *      "cedula": "1014563321",
+ *      "fechaingreso": date,
+ *      "calificacion": 5.0,
+ *      "id": 001,
+ *      "identificacion": "nicolascaceres001",
+ *      "foto": "nicolascaceres.jpg",
+ *      "eps: "millossaludprepagada",
+ *      "pagos"[{
+ *       "id": 91852,
+ *      "estadoTransaccion: "cancelado",
+ *      "fecha": "02/06/2018"}],
+ *      "servicios"[{"costoduracion": 15000, "costodetransporte": 25000, "pago": "paypal", 
+ *      "cliente"[{"id": 001,
+ *      "nombre": nicolascaceres,
+ *      "cedula": 2104565210,
+ *      "fechaDeIngreso": Mayo 21,2017,
+ *      "calificacion": 5.0,
+ *      "pagoAnticipado": false,
+ *      "horasDeServicioSemanal": 5}],
+ *      "id":100, "puntodeencuentro": "usaquen", "puntoderealizacion": "usaquen",
+ *      "exitodiligencia": true,
+ *      "calificacion":5.0, "descripcion": "entregar documentos"
+ *      }]
+ *   }
+ * </pre>   
  * @author n.bello
  */
 public class EmpleadoDetailDTO extends EmpleadoDTO{
@@ -23,7 +69,7 @@ public class EmpleadoDetailDTO extends EmpleadoDTO{
     private ArrayList<ServicioDetailDTO>servicios;
     //CONSTRUCTOR
     /**
-     * 
+     * genera un empleado
      **/
     public EmpleadoDetailDTO()
     {   
@@ -31,19 +77,34 @@ public class EmpleadoDetailDTO extends EmpleadoDTO{
         pagos = new ArrayList<PagoDTO>();
         servicios = new ArrayList<ServicioDetailDTO>();
     }
-
+   
+    /**
+     * Retorna la lista de pagos del empleado
+     * @return la lista de pagos
+     */
     public ArrayList<PagoDTO> getPagos() {
         return pagos;
     }
-
+    /**
+     * se asigna la lista de pagos al empleado
+     * @param pagos Lista de pagos != null
+     */
+    
     public void setPagos(ArrayList<PagoDTO> pagos) {
         this.pagos = pagos;
     }
-
+    /**
+     * retorna la lista de servicios del empleado
+     * @return servicios Lista de servicios del empleado
+     **/ 
     public ArrayList<ServicioDetailDTO> getServicios() {
         return servicios;
     }
-
+    /**
+     * Lista de servicios que se le va a asignar 
+     * @param servicios llega la lista de servicios para asignar !=null
+     */
+    
     public void setServicios(ArrayList<ServicioDetailDTO> servicios) {
         this.servicios = servicios;
     }
