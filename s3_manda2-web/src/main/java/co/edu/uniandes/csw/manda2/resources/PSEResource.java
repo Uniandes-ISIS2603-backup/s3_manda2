c987752a73d914331453c82607e3727fa1fa0010
na.caceres <na.caceres@ISIS2603S3-0074.sis.virtual.uniandes.edu.co>
2018-02-15 07:59:53 -0500

    Conecta con postman pero no funcionan los arrays.

--- s3_manda2-web/src/main/java/co/edu/uniandes/csw/manda2/resources/PSEResource.java
+++ s3_manda2-web/src/main/java/co/edu/uniandes/csw/manda2/resources/PSEResource.java
@@ -16,6 +16,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import co.edu.uniandes.csw.manda2.dtos.PSEDTO;
+import java.util.ArrayList;
+import javax.enterprise.context.RequestScoped;
 /**
  *
  * @author na.caceres
@@ -23,9 +25,19 @@ import co.edu.uniandes.csw.manda2.dtos.PSEDTO;
 @Path("pses")
 @Produces("application/json")
 @Consumes("application/json")
-
+@RequestScoped
 public class PSEResource {
-
+    /**
+     * <h1>GET /api/pses : Obtener todas los pses.</h1>
+     * 
+     * <pre>Busca y devuelve todas las pses que existen en la aplicacion.
+     * 
+     * Codigos de respuesta:
+     * <code style="color: mediumseagreen; background-color: #eaffe0;">
+     * 200 OK Devuelve todas los pses de la aplicacion.</code> 
+     * </pre>
+     * @return JSONArray {@link PSEDTO} - Los pses encontradas en la aplicación. Si no hay ninguna retorna una lista vacía.
+     */
     @GET
     public List<PSEDTO> getPSEs() {
         return null;
