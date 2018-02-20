8f1370d9b0a5b7df277498e8b4a80b0d2c820462
na.caceres <na.caceres@ISIS2603S3-0074.sis.virtual.uniandes.edu.co>
2018-02-20 16:06:38 -0500

    Funcionan todos los test de na.caceres

--- s3_manda2-back/src/test/java/co/edu/uniandes/csw/manda2/test/persistence/PSEPersistenceTest.java
+++ s3_manda2-back/src/test/java/co/edu/uniandes/csw/manda2/test/persistence/PSEPersistenceTest.java
@@ -7,11 +7,18 @@ package co.edu.uniandes.csw.manda2.test.persistence;
 
 import co.edu.uniandes.csw.manda2.entities.PSEEntity;
 import co.edu.uniandes.csw.manda2.persistence.PSEPersistence;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
@@ -23,6 +30,21 @@ import uk.co.jemos.podam.api.PodamFactoryImpl;
  */
 @RunWith(Arquillian.class)
 public class PSEPersistenceTest {
+    /**
+     *
+     * @return Devuelve el jar que Arquillian va a desplegar en el Glassfish
+     * embebido. El jar contiene las clases de Editorial, el descriptor de la
+     * base de datos y el archivo beans.xml para resolver la inyección de
+     * dependencias.
+     */
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(PSEEntity.class.getPackage())
+                .addPackage(PSEPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
     /**
      * Inyección de la dependencia a la clase EditorialPersistence cuyos métodos
      * se van a probar.
@@ -36,6 +58,67 @@ public class PSEPersistenceTest {
      */
     @PersistenceContext
     private EntityManager em;
+    /**
+     * Variable para martcar las transacciones del em anterior cuando se
+     * crean/borran datos para las pruebas.
+     */
+    @Inject
+    UserTransaction utx;
+
+    /**
+     * Configuración inicial de la prueba.
+     *
+     *
+     */
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Limpia las tablas que están implicadas en la prueba.
+     *
+     *
+     */
+    private void clearData() {
+        em.createQuery("delete from ClienteEntity").executeUpdate();
+    }
+
+    /**
+     * lista que tiene los datos de prueba
+     */
+    private List<PSEEntity> data = new ArrayList<PSEEntity>();
+
+    /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las
+     * pruebas.
+     *
+     *
+     */
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+            
+            PSEEntity entity = factory.manufacturePojo(PSEEntity.class);
+
+            em.persist(entity);
+            
+            data.add(entity);
+        }
+    }
     /**
      * Prueba para crear un Editorial.
      *
--- s3_manda2-back/src/test/java/co/edu/uniandes/csw/manda2/test/persistence/TarjetaDeCreditoPersistenceTest.java
+++ s3_manda2-back/src/test/java/co/edu/uniandes/csw/manda2/test/persistence/TarjetaDeCreditoPersistenceTest.java
@@ -7,11 +7,18 @@ package co.edu.uniandes.csw.manda2.test.persistence;
 
 import co.edu.uniandes.csw.manda2.entities.TarjetaDeCreditoEntity;
 import co.edu.uniandes.csw.manda2.persistence.TarjetaDeCreditoPersistence;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
@@ -23,6 +30,21 @@ import uk.co.jemos.podam.api.PodamFactoryImpl;
  */
 @RunWith(Arquillian.class)
 public class TarjetaDeCreditoPersistenceTest {
+    /**
+     *
+     * @return Devuelve el jar que Arquillian va a desplegar en el Glassfish
+     * embebido. El jar contiene las clases de Editorial, el descriptor de la
+     * base de datos y el archivo beans.xml para resolver la inyección de
+     * dependencias.
+     */
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(TarjetaDeCreditoEntity.class.getPackage())
+                .addPackage(TarjetaDeCreditoPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
     /**
      * Inyección de la dependencia a la clase EditorialPersistence cuyos métodos
      * se van a probar.
@@ -37,7 +59,68 @@ public class TarjetaDeCreditoPersistenceTest {
     @PersistenceContext
     private EntityManager em;
     /**
-     * Prueba para crear un Editorial.
+     * Variable para martcar las transacciones del em anterior cuando se
+     * crean/borran datos para las pruebas.
+     */
+    @Inject
+    UserTransaction utx;
+
+    /**
+     * Configuración inicial de la prueba.
+     *
+     *
+     */
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Limpia las tablas que están implicadas en la prueba.
+     *
+     *
+     */
+    private void clearData() {
+        em.createQuery("delete from ClienteEntity").executeUpdate();
+    }
+
+    /**
+     * lista que tiene los datos de prueba
+     */
+    private List<TarjetaDeCreditoEntity> data = new ArrayList<TarjetaDeCreditoEntity>();
+
+    /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las
+     * pruebas.
+     *
+     *
+     */
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+            
+            TarjetaDeCreditoEntity entity = factory.manufacturePojo(TarjetaDeCreditoEntity.class);
+
+            em.persist(entity);
+            
+            data.add(entity);
+        }
+    }
+    /**
+     * Prueba para crear una Tarjeta de credito.
      *
      *
      */
