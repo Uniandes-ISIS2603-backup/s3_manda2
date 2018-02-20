d35bc6df187f27e47b254d365d837eaa8acb5b61
NICOLAS BELLO LUGO <n.bello@sis.virtual.uniandes.edu.co>
2018-02-20 10:20:39 -0500

    se hace el test empleado

---
+++ s3_manda2-back/src/test/java/co/edu/uniandes/csw/manda2/test/persistence/EmpleadoPersistenceTest.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.manda2.test.persistence;
+
+import co.edu.uniandes.csw.manda2.entities.EmpleadoEntity;
+import co.edu.uniandes.csw.manda2.persistence.EmpleadoPersistence;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+/**
+ *
+ * @author n.bello
+ */
+@RunWith(Arquillian.class)
+public class EmpleadoPersistenceTest {
+    /**
+     *
+     * @return Devuelve el jar que Arquillian va a desplegar en el Glassfish
+     * embebido. El jar contiene las clases de Editorial, el descriptor de la
+     * base de datos y el archivo beans.xml para resolver la inyección de
+     * dependencias.
+     */
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(EmpleadoEntity.class.getPackage())
+                .addPackage(EmpleadoPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+    
+    /**
+     * Inyección de la dependencia a la clase EditorialPersistence cuyos métodos
+     * se van a probar.
+     */
+    @Inject
+    private EmpleadoPersistence empleadoPersistence;
+    /**
+     * Contexto de Persistencia que se va a utilizar para acceder a la Base de
+     * datos por fuera de los métodos que se están probando.
+     */
+    @PersistenceContext
+    private EntityManager em;
+    
+    /**
+     * Prueba para crear un Editorial.
+     *
+     *
+     */
+    @Test
+    public void createEmpleadoTest() {
+        PodamFactory factory = new PodamFactoryImpl();
+        EmpleadoEntity newEntity = factory.manufacturePojo(EmpleadoEntity.class);
+        EmpleadoEntity result = empleadoPersistence.create(newEntity);
+
+        Assert.assertNotNull(result);
+
+        EmpleadoEntity entity = em.find(EmpleadoEntity.class, result.getId());
+
+        Assert.assertEquals(newEntity.getNombre(), entity.getNombre());
+    }
+}
