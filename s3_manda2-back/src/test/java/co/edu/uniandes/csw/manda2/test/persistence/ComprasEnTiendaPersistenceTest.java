3df49960e3897f0804aea5c6c7035186192c8c54
Diana Vanessa Gonzalez <dv.gonzalez10@uniandes.edu.co>
2018-02-24 14:37:46 -0500

    tests completos

--- s3_manda2-back/src/test/java/co/edu/uniandes/csw/manda2/test/persistence/ComprasEnTiendaPersistenceTest.java
+++ s3_manda2-back/src/test/java/co/edu/uniandes/csw/manda2/test/persistence/ComprasEnTiendaPersistenceTest.java
@@ -7,14 +7,22 @@ package co.edu.uniandes.csw.manda2.test.persistence;
 
 import co.edu.uniandes.csw.manda2.entities.ComprasEnTiendaEntity;
 import co.edu.uniandes.csw.manda2.persistence.ComprasEnTiendaPersistence;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
 
 /**
  *
@@ -37,8 +45,66 @@ public class ComprasEnTiendaPersistenceTest {
     
     
     @Inject
-    private ComprasEnTiendaPersistence vueltasConDemoraEnOfcinaPersistence;
+    private ComprasEnTiendaPersistence comprasEnTiendaPersistence;
     
     @PersistenceContext
     private EntityManager em;
+    
+    @Inject
+    UserTransaction utx;
+    
+     /**
+     * Limpia las tablas que están implicadas en la prueba
+     */
+     private void clearData() {
+        em.createQuery("delete from ComprasEnTiendaEntity").executeUpdate();
+    }
+     
+     private List<ComprasEnTiendaEntity> data = new ArrayList<ComprasEnTiendaEntity>();
+     
+     
+     
+     private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+            ComprasEnTiendaEntity entity = factory.manufacturePojo(ComprasEnTiendaEntity.class);
+            
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
+     
+     @Before
+    public void configTest(){
+        try{
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    
+    
+    @Test
+     public void createServicioTest(){
+        PodamFactory factory = new PodamFactoryImpl();
+        ComprasEnTiendaEntity newEntity = factory.manufacturePojo(ComprasEnTiendaEntity.class);
+        ComprasEnTiendaEntity result = comprasEnTiendaPersistence.create(newEntity);
+        
+        Assert.assertNotNull(result);
+        
+        ComprasEnTiendaEntity entity = em.find(ComprasEnTiendaEntity.class, result.getId());
+        Assert.assertEquals(newEntity.getCostoDeTransporte(), entity.getCostoDeTransporte());
+        
+    }
 }
--- s3_manda2-back/src/test/java/co/edu/uniandes/csw/manda2/test/persistence/EntregasDeDocumentosPersistenceTest.java
+++ s3_manda2-back/src/test/java/co/edu/uniandes/csw/manda2/test/persistence/EntregasDeDocumentosPersistenceTest.java
@@ -7,14 +7,22 @@ package co.edu.uniandes.csw.manda2.test.persistence;
 
 import co.edu.uniandes.csw.manda2.entities.EntregasDeDocumentosEntity;
 import co.edu.uniandes.csw.manda2.persistence.EntregasDeDocumentosPersistence;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
 
 /**
  *
@@ -39,4 +47,62 @@ public class EntregasDeDocumentosPersistenceTest {
     @PersistenceContext
     private EntityManager em;
     
+    
+    @Inject
+    UserTransaction utx;
+    
+        private List<EntregasDeDocumentosEntity> data = new ArrayList<EntregasDeDocumentosEntity>();
+    
+    
+     /**
+     * Limpia las tablas que están implicadas en la prueba
+     */
+     private void clearData() {
+        em.createQuery("delete from EntregasDeDocumentosEntity").executeUpdate();
+    }
+     
+     private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+            EntregasDeDocumentosEntity entity = factory.manufacturePojo(EntregasDeDocumentosEntity.class);
+            
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
+     
+     
+     @Before
+    public void configTest(){
+        try{
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    
+    @Test
+     public void createServicioTest(){
+        PodamFactory factory = new PodamFactoryImpl();
+        EntregasDeDocumentosEntity newEntity = factory.manufacturePojo(EntregasDeDocumentosEntity.class);
+        EntregasDeDocumentosEntity result = entregasDeDocumentosPersistence.create(newEntity);
+        
+        Assert.assertNotNull(result);
+        
+        EntregasDeDocumentosEntity entity = em.find(EntregasDeDocumentosEntity.class, result.getId());
+        
+         Assert.assertEquals(newEntity.getCostoDeTransporte(), entity.getCostoDeTransporte());
+          Assert.assertEquals(newEntity.getPorcentajeExtra(), entity.getPorcentajeExtra());
+    }
 }
