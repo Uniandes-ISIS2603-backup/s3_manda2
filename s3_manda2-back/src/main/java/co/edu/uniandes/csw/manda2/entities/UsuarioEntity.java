74453f4b51550098121a1197617441c8beac7a97
NICOLAS BELLO LUGO <n.bello@sis.virtual.uniandes.edu.co>
2018-02-26 21:08:56 -0500

    se completa back usuario

--- s3_manda2-back/src/main/java/co/edu/uniandes/csw/manda2/entities/UsuarioEntity.java
+++ s3_manda2-back/src/main/java/co/edu/uniandes/csw/manda2/entities/UsuarioEntity.java
@@ -7,6 +7,7 @@ package co.edu.uniandes.csw.manda2.entities;
 
 import java.io.Serializable;
 import java.util.Date;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -18,8 +19,8 @@ import javax.persistence.TemporalType;
  *
  * @author n.bello
  */
-@MappedSuperclass
-public class UsuarioEntity implements Serializable{
+@Entity
+public abstract class UsuarioEntity implements Serializable{
    @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
