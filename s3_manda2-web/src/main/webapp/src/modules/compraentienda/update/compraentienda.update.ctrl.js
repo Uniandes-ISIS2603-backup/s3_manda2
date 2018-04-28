d1eb81d4aaa16762a67b3fc61a2d48b2d50a85a0
dv.gonzalez10@uniandes.edu.co <dv.gonzalez10@uniandes.edu.co>
2018-04-28 11:53:50 -0500

    fin

--- s3_manda2-web/src/main/webapp/src/modules/compraentienda/update/compraentienda.update.ctrl.js
+++ s3_manda2-web/src/main/webapp/src/modules/compraentienda/update/compraentienda.update.ctrl.js
@@ -1,7 +1,16 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+(function(ng){
+     var mod = ng.module('compraModule');
+     mod.constant("compraContext", "api/comprasentienda");
+     mod.controller('compraDeleteCtrl', ['$scope', '$http', 'vueltaContext', '$state','$rootScope',
+          function ($scope, $http, compraContext, $state, $rootScope){
+              $rootScope.update = true;
+               var idCompra = $state.params.idCompra;
+               $scope.data = {'id':1};
+               
+                $scope.createCompra = function(){
+                    $http.put(compraContext, $scope.data).then(function (response){
+                   $state.go('compraentiendaList', {idCompra: response.id}, {reload:true});
+  });
+                };
+            }]);
+})(window.angular);
