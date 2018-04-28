fb4d227440dd4ab2334480ba02277e7056cf4bbe
Diana Vanessa Gonzalez <dv.gonzalez10@uniandes.edu.co>
2018-04-28 10:14:45 -0500

    bu

---
+++ s3_manda2-web/src/main/webapp/src/modules/vueltascondemoraenoficina/delete/vueltacondemora.delete.ctrl.js
@@ -0,0 +1,14 @@
+(function(ng){
+     var mod = ng.module('vueltaModule');
+     mod.constant("vueltaContext", "api/vueltascondemoraenoficina");
+     mod.controller('vueltaDeleteCtrl', ['$scope', '$http', 'vueltaContext', '$state',
+         function ($scope, $http, vueltaContext, $state){
+             var idVuelta = $state.params.idVuelta;
+             
+             $scope.deleteVuelta = function(){
+                    $http.delete(vueltaContext + '/'+ idVuelta, {}).then(function (response){
+                   $state.go('vueltacondemoraList', {idVuelta: response.id}, {reload:true});
+          });
+            };
+        }]);
+})(window.angular);
