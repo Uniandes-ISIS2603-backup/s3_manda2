70e470a9c2efc6e9d96b7f14ecae3a66e059c677
Diego Ramos <da.ramos@uniandes.edu.co>
2018-05-16 17:23:26 -0500

    test

--- s3_manda2-web/src/main/webapp/src/modules/clientes/clientes.mod.js
+++ s3_manda2-web/src/main/webapp/src/modules/clientes/clientes.mod.js
@@ -4,102 +4,102 @@
     mod.constant("clientesContext", "api/clientes");
     mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
 
-        var basePath = 'src/modules/clientes/';
+            var basePath = 'src/modules/clientes/';
 
-        $stateProvider.state('clientes', {
-            url: '/clientes',
-            abstract: true,
-            views: {
-                'clienteView': {
-                    templateUrl: basePath + 'clientes.html',
-                    controller: 'clienteCtrl',
-                    controllerAs: 'ctrl'
+            $stateProvider.state('clientes', {
+                url: '/clientes',
+                abstract: true,
+                views: {
+                    'clienteView': {
+                        templateUrl: basePath + 'clientes.html',
+                        controller: 'clienteCtrl',
+                        controllerAs: 'ctrl'
+                    }
                 }
-            }
-        });
+            });
 
-        $stateProvider.state('clientesList', {
+            $stateProvider.state('clientesList', {
 
-            url: '/list',
-            parent: 'clientes',
-            views: {
-                'listView': {
-                    templateUrl: basePath + 'clientes.list.html',
+                url: '/list',
+                parent: 'clientes',
+                views: {
+                    'listView': {
+                        templateUrl: basePath + 'clientes.list.html',
+                    }
                 }
-            }
-        });
-        $stateProvider.state('clienteDetail', {
-            url: '/{clienteId:int}/detail',
-            parent: 'clientes',
-            param: {
-                clienteId: null
-            },
-            views: {
-                'listView': {
-                    templateUrl: basePath + 'clientes.list.html',
-                    controller: 'clienteCtrl',
-                    controllerAs: 'ctrl'
+            });
+            $stateProvider.state('clienteDetail', {
+                url: '/{clienteId:int}/detail',
+                parent: 'clientes',
+                param: {
+                    clienteId: null
                 },
-                'detailView': {
-                    templateUrl: basePath + 'cliente.detail.html'
-                }
-            }
-
-        });
-        $stateProvider.state('clienteSubDetail', {
-            url: '/datosPersonales',
-            parent: 'clienteDetail',
-            views: {
-                'clienteView': {
-                    templateUrl: basePath + 'cliente.SubDetail.html',
-                    controller: 'clienteDetailCtrl',
-                    controllerAs: 'ctrl'
+                views: {
+                    'listView': {
+                        templateUrl: basePath + 'clientes.list.html',
+                        controller: 'clienteCtrl',
+                        controllerAs: 'ctrl'
+                    },
+                    'detailView': {
+                        templateUrl: basePath + 'cliente.detail.html'
+                    }
                 }
 
-            }
+            });
+            $stateProvider.state('clienteSubDetail', {
+                url: '/datosPersonales',
+                parent: 'clienteDetail',
+                views: {
+                    'clienteView': {
+                        templateUrl: basePath + 'cliente.SubDetail.html',
+                        controller: 'clienteDetailCtrl',
+                        controllerAs: 'ctrl'
+                    }
 
-        });
-        $stateProvider.state('clientesCreate', {
-            url: '/create',
-            parent: 'clientes',
-            views: {
-                'listView': {
-                    templateUrl: basePath + '/new/clientes.new.html',
-                    controller: 'clienteNewCtrl'
                 }
 
-            }
+            });
+            $stateProvider.state('clientesCreate', {
+                url: '/create',
+                parent: 'clientes',
+                views: {
+                    'listView': {
+                        templateUrl: basePath + '/new/clientes.new.html',
+                        controller: 'clienteNewCtrl'
+                    }
 
-        });
-        $stateProvider.state('clienteUpdate', {
-            url: '/update/{clienteId:int}',
-            parent: 'clientes',
-            param: {
-                clienteId: null
-            },
-            views: {
-                'listView': {
-                    templateUrl: basePath + '/new/clientes.new.html',
-                    controller: 'clienteUpdateCtrl'
                 }
 
-            }
+            });
+            $stateProvider.state('clienteUpdate', {
+                url: '/update/{clienteId:int}',
+                parent: 'clientes',
+                param: {
+                    clienteId: null
+                },
+                views: {
+                    'listView': {
+                        templateUrl: basePath + '/new/clientes.new.html',
+                        controller: 'clienteUpdateCtrl'
+                    }
 
-        });
-        $stateProvider.state('clienteDelete', {
-            url: '/delete/{clienteId:int}',
-            parent: 'clientes',
-            param: {
-                clienteId: null
-            },
-            views: {
-                'listView': {
-                    templateUrl: basePath + '/delete/clientes.delete.html',
-                    controller: 'clienteDeleteCtrl'
                 }
 
-            }
+            });
+            $stateProvider.state('clienteDelete', {
+                url: '/delete/{clienteId:int}',
+                parent: 'clientes',
+                param: {
+                    clienteId: null
+                },
+                views: {
+                    'listView': {
+                        templateUrl: basePath + '/delete/clientes.delete.html',
+                        controller: 'clienteDeleteCtrl'
+                    }
 
-        });
-    }]);
+                }
+
+            });
+        }]);
 })(window.angular);
