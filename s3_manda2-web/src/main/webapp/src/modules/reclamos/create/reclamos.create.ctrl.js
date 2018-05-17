89fdb490552e3521bafbc86e80da475f635bb0a4
na.caceres <na.caceres@ISIS2603S3-0074.sis.virtual.uniandes.edu.co>
2018-05-17 05:19:54 -0500

    Arreglo de reclamo front

--- s3_manda2-web/src/main/webapp/src/modules/reclamos/create/reclamos.create.ctrl.js
+++ s3_manda2-web/src/main/webapp/src/modules/reclamos/create/reclamos.create.ctrl.js
@@ -2,18 +2,18 @@
     function (ng) {
         var mod = ng.module('reclamoModule');
         mod.constant('reclamoContext', 'api/reclamos');
-        mod.controller('reclamoCreateCtrl', ['$scope', '$http', 'reclamoContext', '$state',
-            function ($scope, $http, reclamoContext, $state) {
+        mod.controller('reclamoCreateCtrl', ['$scope', '$http', 'reclamoContext', '$state','$rootScope',
+            function ($scope, $http, reclamoContext, $state,$rootScope) {
+                $rootScope.edit = false;
                 $scope.data = {};
                 $scope.createReclamo = function () {
-                    console.log(1);
                     $http.post(reclamoContext, {
                             id: $scope.reclamoId,
                             numero: $scope.reclamoNumero,
                             mensaje: $scope.reclamoMensaje
                         }
                     ).then(function (response) {
-                        $state.go('reclamoList', {idReclamo: response.id}, {reload: true});
+                        $state.go('reclamosList', {idReclamo: response.id}, {reload: true});
                     });
                 };
             }]);
