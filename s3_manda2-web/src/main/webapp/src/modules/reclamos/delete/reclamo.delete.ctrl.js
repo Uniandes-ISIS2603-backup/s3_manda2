d8df14a8feed309e33204e636d8d1308193b69b1
NICOLAS BELLO LUGO <n.bello@sis.virtual.uniandes.edu.co>
2018-05-17 06:26:40 -0500

    arregla reclamos

--- s3_manda2-web/src/main/webapp/src/modules/reclamos/delete/reclamo.delete.ctrl.js
+++ s3_manda2-web/src/main/webapp/src/modules/reclamos/delete/reclamo.delete.ctrl.js
@@ -4,9 +4,12 @@
         mod.controller('reclamoDeleteCtrl', ['$scope', '$http', 'reclamoContext', '$state',
 
             function ($scope, $http, reclamoContext, $state) {
+                console.log($state.params.reclamoId);
                 var idReclamo = $state.params.reclamoId;
 
                 $scope.deleteReclamo = function () {
+                    console.log(reclamoContext);
+                    console.log(idReclamo);
                     $http.delete(reclamoContext + '/' + idReclamo, {}).then(function (response) {
                         $state.go('reclamosList', {reclamoId: response.data.id}, {reload: true});
                     });
